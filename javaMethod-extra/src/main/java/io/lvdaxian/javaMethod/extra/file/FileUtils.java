9407a2bded36586b44a30a1567a38c2b96a5a1ca
全栈切图仔 <40829727+a572251465@users.noreply.github.com>
2024-08-13 16:21:26 +0800

    feat: 添加文件 工具类的方法

--- javaMethod-extra/src/main/java/io/lvdaxian/javaMethod/extra/file/FileUtils.java
+++ javaMethod-extra/src/main/java/io/lvdaxian/javaMethod/extra/file/FileUtils.java
@@ -1,5 +1,24 @@
 package io.lvdaxian.javaMethod.extra.file;
 
-public class FileUtils {
 
+import cn.hutool.core.util.StrUtil;
+
+import java.io.File;
+
+public class FileUtils {
+  public static boolean ensureDir(String dirPath) {
+    if (StrUtil.isEmpty(dirPath))
+      return false;
+    
+    File file = new File(dirPath);
+    // 文件存在
+    if (file.exists()) {
+      // 文件不是目录
+      if (!file.isDirectory()) return false;
+    } else
+      // 能执行到这里，文件不存在
+      file.mkdir();
+    
+    return true;
+  }
 }
